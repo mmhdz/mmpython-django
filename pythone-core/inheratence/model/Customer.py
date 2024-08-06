fae0f7c7884ccf04ae93f72c9a65b2ed0b005dff
MIhael Hadzhiev <mihael.hadzhiev@mentormate.com>
2024-08-06 18:56:12 +0300

    create Customer and Employee that inherit User base class

---
+++ pythone-core/inheratence/model/Customer.py
@@ -0,0 +1,13 @@
+from inheratence.model.User import User
+from inheratence.model.enums.UserType import UserType
+
+class Customer(User):
+    def __init__(self, first_name: str, last_name: str):
+        super().__init__(first_name, last_name)
+        self.type = UserType.CUSTOMER
+
+    def get_full_name_and_type_dict(self) -> dict:
+        return {'full_name': super().get_full_name(), 'user_type': self.type.name}
+
+
+
