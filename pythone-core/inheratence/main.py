8d21a65d5850cdfcf6532ed3bfa584606b9d0fe4
MIhael Hadzhiev <mihael.hadzhiev@mentormate.com>
2024-08-07 12:30:41 +0300

    add externel customer model which does not inherit User base class

--- pythone-core/inheratence/main.py
+++ pythone-core/inheratence/main.py
@@ -1,12 +1,14 @@
 from inheratence.model.Employee import Employee
 from inheratence.model.Customer import Customer
+from inheratence.model.ExternalCustomer import ExternalCustomer
 from inheratence.model.ManagementSystem import ManagementSystem
 
 
 employee = Employee("EmployeeFirstName", "LastName")
 customer = Customer("IamCustomer", "LastName")
+external_customer = ExternalCustomer("IamExternalCustomer", "LastName")
 
-users_list = [employee, customer]
+users_list = [employee, customer, external_customer]
 
 management_system = ManagementSystem()
 management_system.print_user_data(users_list)
--- pythone-core/inheratence/model/enums/UserType.py
+++ pythone-core/inheratence/model/enums/UserType.py
@@ -4,4 +4,5 @@ from enum import Enum
 class UserType(Enum):
     CUSTOMER = 1,
     Employee = 2,
-    Admin = 3
+    ExternalCustomer = 3
+    Admin = 4
