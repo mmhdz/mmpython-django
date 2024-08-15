3dbdba459d259c666b1c9b80ed056777faf12830
MIhael Hadzhiev <mihael.hadzhiev@mentormate.com>
2024-08-15 12:39:57 +0300

    configure settings for Django Debug Tool

--- django-basics/mm_blog/mm_blog/settings.py
+++ django-basics/mm_blog/mm_blog/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mm_blog.urls'
@@ -124,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'blog_post_app.User'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
