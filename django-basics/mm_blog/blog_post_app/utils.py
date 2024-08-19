c684b96578b6986613c4ed8f98d0aa16eaaffa3f
mmhdz <mihael.hadzhiev@mentormate.com>
2024-08-19 10:53:14 +0300

    Delete django-basics/mm_blog directory

--- django-basics/mm_blog/blog_post_app/utils.py
+++
@@ -1,7 +0,0 @@
-class Utils:
-
-    @staticmethod
-    def modify_hashtag_raw_string(hashtags_string: str):
-        hashtags = hashtags_string.replace(" ", ",").split(",")
-
-        return [x for x in hashtags if len(x) > 0]
